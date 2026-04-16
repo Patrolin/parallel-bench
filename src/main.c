c03759a243ae0ed10ebb03d0b07e50ec5045ecf0
Patrolin <patrik@balas.cz>
2026-04-17 00:13:40 +0200

    cleanup

--- src/main.c
+++ src/main.c
@@ -3,53 +3,57 @@
 #include "lib/mem.h"
 #include "lib/threads.h"
 
+// params
+#define WARMUP_COUNT      1000
+#define REPEAT_COUNT      1 * Mega
+#define REPEAT_GROUP_SIZE 1000
+ASSERT((WARMUP_COUNT + REPEAT_COUNT) % REPEAT_GROUP_SIZE == 0);
+
 // timings
-#define WARMUP_COUNT                100
-#define REPEAT_COUNT                10 * Mega
-#define REPEAT_GROUP_SIZE           1000
 #define repeat(user_data, callback) repeat_impl(t, user_data, callback, string(#callback));
 void repeat_impl(Thread t, rawptr user_data, void (*callback)(Thread t, rawptr user_data), string name) {
   // get the repeat count
   u64 threads_start = global_threads.thread_infos[t].threads_start;
   u64 threads_end = global_threads.thread_infos[t].threads_end;
   u64 thread_count = threads_end - threads_start;
-  u64 repetition_count = (REPEAT_COUNT / thread_count) + (t > (REPEAT_COUNT % thread_count));
   // repeat n times
   u64 cycles_count = 0;
   u64 cycles_sum = 0;
   u64 cycles_max = 0;
-  for (u64 i = 0; i < repetition_count + WARMUP_COUNT; i += REPEAT_GROUP_SIZE) {
+  for (u64 i = 0; i < WARMUP_COUNT + REPEAT_COUNT; i += REPEAT_GROUP_SIZE) {
     // time `REPEAT_GROUP_SIZE` runs
-    u64 cycles_times[REPEAT_GROUP_SIZE];
+    u64 cycle_times[REPEAT_GROUP_SIZE];
     for (u64 j = 0; j < REPEAT_GROUP_SIZE; j++) {
       u64 cycles_before = read_cycle_counter();
       callback(t, user_data);
       u64 cycles_after = read_cycle_counter();
-      cycles_times[j] = cycles_after - cycles_before;
+      cycle_times[j] = cycles_after - cycles_before;
     }
     // sort the times
     for (u64 j = 1; j < REPEAT_GROUP_SIZE; j++) {
       u64 k = j;
-      u64 current = cycles_times[k];
+      u64 current = cycle_times[k];
       for (; k > 0; k--) {
-        u64 prev = cycles_times[k - 1];
-        if (prev > current) cycles_times[k] = prev;
+        u64 prev = cycle_times[k - 1];
+        if (prev > current) cycle_times[k] = prev;
         else break;
       }
-      cycles_times[k] = current;
+      cycle_times[k] = current;
     }
     // store the lowest 50% (don't time OS interrupts)
-    for (u64 j = 0; j < REPEAT_GROUP_SIZE / 2; j++) {
-      cycles_count += 1;
-      u64 dcycles = cycles_times[j];
-      cycles_sum += dcycles;
-      if (dcycles > cycles_max) cycles_max = dcycles;
+    if (i >= WARMUP_COUNT) {
+      for (u64 j = 0; j < REPEAT_GROUP_SIZE / 2; j++) {
+        cycles_count += 1;
+        u64 dcycles = cycle_times[j];
+        cycles_sum += dcycles;
+        if (dcycles > cycles_max) cycles_max = dcycles;
+      }
     }
   }
   // print result
   if (single_core(t)) {
     f64 cycles_mean = f64(cycles_sum) / f64(cycles_count);
-    printfln("%: % cy (% cy)", string, name, u64, u64(cycles_mean), u64, cycles_max);
+    printfln("  %: % cy (% cy)", string, name, u64, u64(cycles_mean), u64, cycles_max);
   }
   barrier(t);
 }
@@ -89,6 +93,6 @@ int main() {
   _init_page_fault_handler();
   // start threads and do work
   u32 thread_count = _get_logical_core_count();
-  // thread_count = 64;
+  // thread_count = 1;
   _start_threads(thread_count);
 }
