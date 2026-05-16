838bbb141698b2353558f83207fe76f5e05fb7a4
Patrolin <patrik@balas.cz>
2026-05-16 16:35:58 +0200

    c

--- src/lib/math.h
+++ src/lib/math.h
@@ -1,18 +1,15 @@
 #pragma once
 #include "builtin.h"
 
+// TODO: better math functions that don't error...
 #define sin(x) _Generic((x), \
   f32: __builtin_sinf(x),    \
   f64: __builtin_sin(x))
 #define cos(x) _Generic((x), \
   f32: __builtin_cosf(x),    \
   f64: __builtin_cos(x))
-#define sqrt(x) _Generic((x), \
-  f32: __builtin_sqrtf(x),    \
-  f64: __builtin_sqrt(x))
-#define cbrt(x) _Generic((x), \
-  f32: __builtin_cbrtf(x),    \
-  f64: __builtin_cbrt(x))
+#define sqrt(x) (assume((x) >= 0), _Generic((x), f32: __builtin_sqrtf(x), f64: __builtin_sqrt(x)))
+#define cbrt(x) (assume((x) >= 0), _Generic((x), f32: __builtin_cbrtf(x), f64: __builtin_cbrt(x)))
 
 // f32 vector
 STRUCT(f32v2) {
@@ -42,20 +39,17 @@ f32v2 normalized(f32v2 a) {
   f32 a_norm = 1 / norm(a);
   return f32v2(a.x * a_norm, a.y * a_norm);
 }
-// f32 rotor
+// f32 normalized rotor
 STRUCT(f32r2) {
-  f32 sc;
   f32 xy;
 };
-f32r2 rotor_between_vectors(f32v2 from, f32v2 to) {
-  return (f32r2){
-    from.x * to.x + from.y * to.y,
-    from.x * to.y - from.y * to.x,
-  };
+f32r2 rotor_from_vectors(f32v2 a, f32v2 b) {
+  return (f32r2){a.x * b.y - a.y * b.x};
 }
 f32v2 rotate(f32v2 a, f32r2 r) {
+  f32 scalar = sqrt(1 - r.xy * r.xy);
   return (f32v2){
-    a.x * r.sc - a.y * r.xy,
-    a.x * r.xy + a.y * r.sc,
+    a.x * scalar - a.y * r.xy,
+    a.x * r.xy + a.y * scalar,
   };
 }
